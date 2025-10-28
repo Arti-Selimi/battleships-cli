a20da9386987e1dc2d5ce6fe5bd58ecb72842026
Arti <arti.selimi@student.cacttus.education>
2025-10-28 19:18:48 +0100

    start fresh

--- client/menu/helpers/shoot.ts
+++
@@ -1,8 +0,0 @@
-import { Matrix } from "../../classes/matrix.ts";
-
-
-export const shoot = (matrix: Matrix) => {
-    console.log("Shoot at the computer's ships by entering the coordinates (row, column).");
-    console.log("For example, to shoot at (2, 3), you would enter: 2 3");
-    matrix.print()
-}
